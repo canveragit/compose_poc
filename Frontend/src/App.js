43292b8d33eaf55df81930b69b4475bd77fa2e4c
Mervin <106072121+Mervin-Canvera@users.noreply.github.com>
2022-10-11 21:53:40 +0530

    Frontend Folder

---
+++ Frontend/src/App.js
@@ -0,0 +1,86 @@
+import './App.scss';
+import { Button, Container, Divider, Stack, TextField } from "@mui/material"
+import React, { useState } from "react";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+class ImageFrame extends React.Component {
+
+  state = { file: "" };
+  handleChange = (e) => {
+    this.setState({ file: URL.createObjectURL(e.target.files[0]) });
+  }
+
+  render() {
+    return (
+      <Row className="justify-content-md-center">
+        <Col md={12} sm={12}>
+          <div className='image-tools'>
+            <p>{this.props.index + 1}</p>
+            <AddPhotoAlternateOutlinedIcon className='icon' onClick={this.props.handleAdd} > </AddPhotoAlternateOutlinedIcon>
+            <DeleteOutlineOutlinedIcon className='icon' onClick={() => this.props.handleRemove(this.props.index)}></DeleteOutlineOutlinedIcon>
+            <KeyboardArrowUpIcon className='icon'></KeyboardArrowUpIcon>
+            <KeyboardArrowDownIcon className='icon'></KeyboardArrowDownIcon>
+          </div>
+          <div className="image-container">
+            <div className='image-preview' style={{ backgroundImage: "url(" + this.state.file + ")" }}></div>
+          </div>
+          <div>
+            <label className='btn-grey' onChange={this.handleChange} htmlFor={"formId-" + this.props.index}>
+              <input name="" type="file" id={"formId-" + this.props.index} hidden />
+              Add Image
+            </label>
+          </div>
+        </Col>
+      </Row>
+    )
+  }
+}
+
+
+export default function App() {
+
+  const [page, setPage] = useState([
+    {
+      pageNumber: ""
+    }
+  ])
+
+  // adds new input
+  const handleAdd = () => {
+    setPage([
+      ...page,
+      {
+        pageNumber: ""
+      }
+    ])
+  }
+  // removes input
+  const handleRemove = (index) => {
+    if (page.length !== 1) {
+      setPage(page.splice(index, 1));
+    }
+  }
+
+  return (
+    <>
+      <Container className="App">
+        <Stack spacing={2}>
+          {page.map((item, index) => (
+            <ImageFrame
+              handleAdd={handleAdd}
+              handleRemove={handleRemove}
+              page={page}
+              index={index}
+            />
+          ))}
+        </Stack>
+
+      </Container>
+    </>
+  )
+}
